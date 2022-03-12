ffbbffb3aebbc296991e5f2a0cffffa0b107af5e
APallais <70729466+APallais@users.noreply.github.com>
2022-03-12 14:26:31 -0600

    metodo operar

--- Operaciones.java
+++ Operaciones.java
@@ -157,4 +157,72 @@ public class Operaciones {
 	    }
 	    return true; //Es numerico
 	}
+	
+    public String operar (String demo){
+        boolean verificador=false;
+        do{
+            //System.out.println(demo);
+            verificador=false;
+            int posicionInicial=0;
+            for(int i=0;i<demo.length();i++){//analizo el string buscando cual es la ultima operacion
+                if((demo.charAt(i)+"").equals("(")){
+                    posicionInicial=i;
+                    verificador=true;
+                }//si no hay operacion ahi se acaba
+            }
+            if(verificador==true){
+                String resupuesta="1";
+                String pregunta="";
+                int posicionFinal=posicionInicial;
+                do{//tengo solo la ultima operacion
+                    posicionFinal++;
+                }while(!((demo.charAt(posicionFinal)+"").equals(")")));
+                boolean buscaSignos=true;
+                String signo="";
+                for (int i=posicionInicial+1;i<posicionFinal;i++){//verifico que signo es
+                    if (buscaSignos==true){
+                        if((demo.charAt(i)+"").equals("+")||(demo.charAt(i)+"").equals("-")||(demo.charAt(i)+"").equals("*")||(demo.charAt(i)+"").equals("/")){
+                            //i++;
+                            signo=demo.charAt(i)+"";
+                            buscaSignos=false;
+                        }
+                    }else{
+                        pregunta=pregunta+demo.charAt(i);//ya visto el signo pongo todos los operandos en una lista
+                    }
+                }
+                do{
+                    String nuevo="";
+                    for(int j=1;j<pregunta.length();j++){
+                        nuevo=nuevo+pregunta.charAt(j);
+                    }
+                    pregunta=nuevo;
+                }while(((pregunta.charAt(0)+"").equals(" ")));
+                switch (signo){//opero la lista
+                    case "+":
+                        System.out.println("la"+pregunta);
+                        resupuesta=suma(pregunta);
+                        break;
+                    case "-":
+                        resupuesta=resta(pregunta);
+                        break;
+                    case "*":
+                        resupuesta=Producto(pregunta);
+                        break;
+                    case "/":
+                        resupuesta=division(pregunta);
+                        break;
+                }
+                String nuevo="";//sustituyo la operacion opeerada por la respuesta
+                for(int i=0;i<posicionInicial;i++){
+                    nuevo=nuevo+demo.charAt(i);
+                }
+                nuevo=nuevo+resupuesta;
+                for(int i=posicionFinal+1;i<demo.length();i++){
+                    nuevo=nuevo+demo.charAt(i);
+                }
+                demo=nuevo;
+            }
+        }while(verificador==true);
+        return demo;
+    }
 }
