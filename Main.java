f65dd269b01e8c1629a737062b9947118cea7ba8
Wilfredo-gallegos <70668558+Wilfredo-gallegos@users.noreply.github.com>
2022-03-12 15:54:33 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 
  */
 
 /**
- * @author ThinkPad T470
+ * @author Wilfredo
  *
  */
 public class Main {
@@ -13,7 +16,49 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		Archivo archivo = new Archivo();
+		Operaciones operaciones = new Operaciones();
+		System.out.println("Ingrese la ruta del archivo .txt a leer:");
+		String ruta = teclado.nextLine();
+		String texto = archivo.Leer(ruta);
+		ArrayList<String> lista = archivo.Listar(texto);
+		for(int i=0; i<lista.size();i++) {
+			if(lista.get(i).equals(null)) {
+				continue;
+			/*Character.compare(ParseString(lista.get(i)).charAt(1), charAt("+"))
+			char valor =  (char) lista.get(i);
+			String valor1 = ParseString(valor);
+			if(Character.compare(valor1.charAt(1), ("+"))==0||Character.compare(valor1.charAt(1), charAt("-"))==0||Character.compare(valor1.charAt(1), charAt("*"))==0||Character.compare(valor1.charAt(1), charAt("/"))==0) {
+				System.out.println((((String) lista.get(i)).charAt(1)));*/
+			}else if(lista.get(i)!=null) {
+				for(int j=0;j<lista.get(i).length();i++) {
+					String.valueOf(lista.get(i).charAt(i)+"");
+				}
+				try{
+					if ((( lista.get(i)).charAt(1)+"").equals("+")||(( lista.get(i)).charAt(1)+"").equals("/")||(( lista.get(i)).charAt(1)+"").equals("-")||(( lista.get(i)).charAt(1)+"").equals("*")) {
+						System.out.println(operaciones.operar(lista.get(i))+"\n");
+					}
+					}catch(Exception e){
+						
+					}
 
+			
+			}else {
+				System.out.println("no");
+			}
+			
+		}
+		
 	}
 
+
 }
+
+
+
+/**texto = "(defvar Fn 10)\n (defvar lista '(1 1))\n" + 
+"(loop for i from 2 to Fn\n" + 
+"    do(setq lista (append (list (* (nth 0 lista) i)) lista))\n" + 
+")\n" + 
+"(print (car lista))";**/
