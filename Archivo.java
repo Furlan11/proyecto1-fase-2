3525bb452e8c16abf01fb108540ab90473f768a0
Furlan11 <mitofurlan13@gmail.com>
2022-03-12 12:54:56 -0600

    hjn

--- Archivo.java
+++ Archivo.java
@@ -1,4 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Archivo {
+	public String Leer(String direccion){ //direccion del archivo
+        
+        String texto = "";
+        
+        try{
+            BufferedReader bf = new BufferedReader(new FileReader(direccion));
+            String temp = "";
+            String bfRead;
+            while((bfRead = bf.readLine()) != null){ 
+                //haz el ciclo, mientras bfRead tiene datos
+                temp = temp + bfRead; //guardado el texto del archivo
+            }
+            
+            texto = temp;
+            
+        }catch(Exception e){ 
+            System.err.println("No se encontro archivo");
+        }
+        
+        return texto;
+       
+           }
+	 public  ArrayList<String> Listar(String demo) {
+	        ArrayList<String> listas = new ArrayList<String>();
+	        int contador=0;
+	        String cosa ="";
+	        for (int i=0;i<demo.length();i++){
+	            
+	            if((demo.charAt(i)+"").equals("\n")){
+	                listas.add(cosa);
+	                cosa="";
+	            }else{
+	                cosa=cosa+demo.charAt(i);
+	            }
+	        }
+	        listas.add(cosa);
+	       return listas;
 
+     	
+     }
+     
+
+    
+	
+	
 }
--- Operaciones.java
+++ Operaciones.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
 
 public class Operaciones {
 
+	public String suma(String sumandos) {
+		String resultado= "";
+		String sumando1="";
+		String sumando2="";
+		int contador=0;
+		int largo=sumandos.length();
+		
+		for(int i=0; i<largo;i++) {
+			String caracter=String.valueOf(sumandos.charAt(i));
+			if(caracter!=" ") {
+				
+					if(contador==0) {
+					 sumando1=sumando1+caracter;
+					 }	
+					if(contador==1) {
+					 sumando2=sumando2+caracter;
+					 return sumando2;}	
+					
+				
+				
+			}
+			if(caracter==" ") {
+				contador=contador++;
+				if(contador==2) {
+					double num1=Double.parseDouble(sumando1);
+					double num2=Double.parseDouble(sumando2);
+					resultado=String.valueOf((num1+num2));
+					
+					sumando1= resultado;
+					
+					contador=1;
+					
+				}
+			}
+			
+			
+		}
+		
+		return resultado;
+		 
+	}
+	
+	/**
+	 * verificar si un caracter se puede volver un numero
+	 * @param ope eel string con el cacrater que se queire validar
+	 *  @return bollean con true si e su numeo
+	 */
+	public static boolean isNumeric(String ope) {
+	    try {
+	        double d = Double.parseDouble(ope);
+	    } catch (NumberFormatException nfe) {
+	        return false; //Error no es numerico
+	    }
+	    return true; //Es numerico
+	}
 }
